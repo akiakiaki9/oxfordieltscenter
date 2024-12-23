415b273ee3103f7e1e251cd271fd424cf87921eb
Akbar <tillayevakbarshoh@gmail.com>
2024-12-23 18:33:45 +0500

    add next sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://oxfordieltscenter.uz/', // Замените на URL вашего сайта
+    generateRobotsTxt: true, // Создаёт robots.txt автоматически
+    sitemapSize: 5000, // Максимум страниц в одном sitemap-файле
+    changefreq: 'daily', // Частота обновления страниц
+    priority: 0.7, // Приоритет страниц по умолчанию
+    exclude: ['/admin', '/private'], // Исключите страницы
+    robotsTxtOptions: {
+        policies: [
+            {
+                userAgent: '*',
+                allow: '/',
+            },
+            {
+                userAgent: 'Googlebot',
+                disallow: ['/private'], // Пример настройки для конкретного бота
+            },
+        ],
+        additionalSitemaps: [
+            'https://oxfordieltscenter.uz/sitemap-1.xml', // Укажите дополнительные карты сайта, если они есть
+        ],
+    },
+};
+
+module.exports = config;
